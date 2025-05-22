18978becf3821832e4834f3e49baec231f90950d
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2025-05-22 12:02:03 -0500

    Exercise 1

--- ClearCodeTutorials/src/main.cpp
+++ ClearCodeTutorials/src/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
+    // Variables
+    Color DARK_RED = {100, 23, 23, 255};
+
     InitWindow(1920, 1080, "Base");
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawPixel(100, 200, {225, 230, 12, 255}); //Draws a  pixel with coordinates manually inputed; (x, y, {color});
-        DrawPixelV({101, 200}, WHITE); //Draws a pizel with coordinates as a vector; ({position}, {color}); position can be 3D
-        DrawCircle(400, 200, 10, GREEN);
-        DrawCircleV({1000, 600}, 200, YELLOW); //({x, y}, radius, {color}); position can be 3D
+        DrawLineEx({0, 0}, {500, 200}, 10, DARK_RED);
         EndDrawing();
     }
     CloseWindow();
