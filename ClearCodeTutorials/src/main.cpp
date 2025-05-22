f25d8bb505418ee8132bc5dbf9f87090600a1473
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2025-05-22 11:53:17 -0500

    Drawing tutorial; getting back in the swing of things

--- ClearCodeTutorials/src/main.cpp
+++ ClearCodeTutorials/src/main.cpp
@@ -8,7 +8,11 @@ int main()
     while(!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(BLACK);
+        DrawPixel(100, 200, {225, 230, 12, 255}); //Draws a  pixel with coordinates manually inputed; (x, y, {color});
+        DrawPixelV({101, 200}, WHITE); //Draws a pizel with coordinates as a vector; ({position}, {color}); position can be 3D
+        DrawCircle(400, 200, 10, GREEN);
+        DrawCircleV({1000, 600}, 200, YELLOW); //({x, y}, radius, {color}); position can be 3D
         EndDrawing();
     }
     CloseWindow();
